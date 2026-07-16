ff91febc13a4404bc0074aec66eb7ca6e95f5d2a
DashPgi <pajiparsa@gmail.com>
2026-07-16 23:32:14 +0330

    second

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("hello World")
+import sys
+from PyQt6.QtWidgets import  QApplication,QWidget,QLineEdit,QPushButton,QTextEdit,QVBoxLayout
+from PyQt6.QtGui import QIcon
+
+class  Myapp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Hello World")
+        self.setWindowIcon(QIcon("icon.png"))
+        self.resize(300,200)
+# app = QApplication([])
+app = QApplication(sys.argv)
+
+window = Myapp()
+window.show()
+app.exec()
