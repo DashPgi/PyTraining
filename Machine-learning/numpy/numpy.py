95f3c1d6ad7c715e6ffb2eff81aa8c60ce4ac1f3
DashPgi <pajiparsa@gmail.com>
2026-08-04 16:54:28 +0330

    setForsumfun

--- Machine-learning/numpy/numpy.py
+++ Machine-learning/numpy/numpy.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-
 # -------- Array's
-OneD = [1,2,3,5,6]
+OneD = [1, 2, 3, 5, 6]
 Array_1D = np.array(OneD)
 
 print(Array_1D)
-print(f"Size : {Array_1D.nbytes}","Child-Type : ",Array_1D.dtype,"Dimentions : ",Array_1D.ndim,"Type :",type(Array_1D))
+print(f"Size : {Array_1D.nbytes}", "Child-Type : ", Array_1D.dtype, "Dimentions : ", Array_1D.ndim, "Type :",
+      type(Array_1D))
 print((Array_1D.reshape(-1, 1)))
 
-TwoD = [[1,2,3],[4,5,6]]
+TwoD = [[1, 2, 3], [4, 5, 6]]
 Array_2D = np.array(TwoD)
 
 print(Array_2D)
@@ -17,7 +17,7 @@ print(f"Size : {Array_2D.nbytes} Child Type : {Array_2D.dtype} Dimetion : {Array
 
 # 1 number = 8 byte
 
-ThreeD = [[[1,2,3],[4,5,6]],[[7,8,9],[7,8,9]]]
+ThreeD = [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [7, 8, 9]]]
 Array_3D = np.array(ThreeD)
 
 # Shape (Plate,Row,Column)
@@ -26,15 +26,15 @@ print(Array_3D.shape)
 print(Array_3D)
 print(f"Size : {Array_3D.nbytes} Child Type : {Array_3D.dtype} Dimetion : {Array_3D.ndim} Type : {type(Array_3D)}")
 
-Array_3D_2 = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[7,8,9]]])
-Array_4D = np.stack([Array_3D_2,Array_3D])
+Array_3D_2 = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [7, 8, 9]]])
+Array_4D = np.stack([Array_3D_2, Array_3D])
 
 print(Array_4D)
 print(f"Shape : {Array_4D.shape}")
 print(f"Size : {Array_4D.nbytes} Child Type : {Array_4D.dtype} Dimetion : {Array_4D.ndim} Type : {type(Array_4D)}")
 
-Array_4D_2 = np.array([Array_3D,Array_3D_2])
-Array_5D = np.stack([Array_4D_2,Array_4D])
+Array_4D_2 = np.array([Array_3D, Array_3D_2])
+Array_5D = np.stack([Array_4D_2, Array_4D])
 
 print(Array_5D)
 print(f"Size : {Array_5D.nbytes} Child Type : {Array_5D.dtype} Dimetion : {Array_5D.ndim} Type : {type(Array_5D)}")
@@ -45,8 +45,26 @@ print(f"Size : {Array_5D.nbytes} Child Type : {Array_5D.dtype} Dimetion : {Array
 # -------- Mathematics function
 
 # set For all indexes
-A = np.array([16,12,-3])
+A = np.array([16, 12, -3])
 print(f"shape : {A.shape}")
 print(f"ABS : {np.abs(A)}")
 print(f"Sqrt : {np.sqrt(A)}")
 print(f"Log : {np.log(A)}")
+print(f"power : {np.power(A, 2)}")
+
+# set For some Indexes (Sensitive To Nan Index)
+# Baraye Delete Nan Bug Mitoonim Fun -> Nanfun => max -> nanmax
+
+B = np.array([[4, 9, -13], A])
+print(f"maxnnumber : {np.max(A)}")
+print(f"maxnumber Index : {np.argmax(A)}")
+print(f"Sum  : {np.sum(A)}")
+print(f"mean  : {np.mean(A)}")
+print(f"median  : {np.median(A)}")
+print(f"std : {np.std(A)}")  # Enheraf  Meyar
+print(f"variance : {np.var(A)}")  # Variance
+print(f"cumsum : {np.cumsum(A)}")  # Jame Tajmii
+print(f"product : {np.prod(A)}")
+print(f"quantile : {np.quantile(A, q=0.9)}")
+print(f"percentile : {np.percentile(A, q=90)}")
+
