cf54d57489bcfd9de4866271e67c6dd308328b42
DashPgi <pajiparsa@gmail.com>
2026-08-05 13:50:33 +0330

    Expending Dimension

--- Machine-learning/numpy/practic.py
+++ Machine-learning/numpy/practic.py
@@ -2,12 +2,17 @@ import numpy as np
 
 A = np.array([[1, 2, 3], [4, 5, 6]])
 
-B = np.array([3])
+B = np.array([[6, 4], [5, 4], [3, 2]])
 
 print(np.shape(A))
 # 2 * 3
 print(np.shape(B))
-# 1*1
-C = A * B
-print(np.shape(C))
-# 2*3
+# 3*2
+
+A_R = A[:, :, np.newaxis]
+B_R = B[np.newaxis, :, :]
+
+C = A_R + B_R
+
+print(C)
+print(C.shape)
