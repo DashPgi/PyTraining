66c11de72275bad360d929c066a0446e8ae33458
DashPgi <pajiparsa@gmail.com>
2026-08-06 15:51:16 +0330

    practic.py

--- Machine-learning/numpy/practic.py
+++ Machine-learning/numpy/practic.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+# need For Studing and Research !!!
+
+rng = np.random.default_rng()
+normal = rng.normal(loc=10,scale=3,size=10)
+uniform = rng.uniform(low=0,high=10,size=10)
+exp = rng.exponential(scale=3,size=10)
+bino = rng.binomial(size=10)
+
+
