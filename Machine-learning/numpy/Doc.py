8335a203205560f722f5b85742f1a17a041a6c2f
DashPgi <pajiparsa@gmail.com>
2026-08-07 18:50:21 +0330

    creation

--- Machine-learning/numpy/Doc.py
+++ Machine-learning/numpy/Doc.py
@@ -42,6 +42,32 @@ print(f"Size : {Array_5D.nbytes} Child Type : {Array_5D.dtype} Dimetion : {Array
 # Shape (Tedade 4D,Tedade 3D,Tedade Row,Tedade Column)
 # stack Array 1 + Array 1 = Array 2 => Nazarie Abaad Hendesi inja Bedard Nmikhore !!
 
+
+# 1D Array Creation
+A = np.arange(3) # range for generate number in araray
+B = np.arange(0,20,2)
+C = np.linspace(1,4,1)
+
+print(C)
+print(B)
+print(A)
+
+# 2D Array Creation
+
+A2 = np.eye(3)
+B2 = np.eye(2,3)
+print(A2)
+print(B2)
+
+C2 = np.diag([1,2,3]) # for diagonal
+print(C2)
+D2 = np.array([[1,2,3],[4,5,6]])
+print(np.diag(D2)) # show D2's diagonal
+
+D = np.indices((3,4))
+
+print(D)
+
 # -------- Mathematics function
 
 # set For all indexes
--- Machine-learning/numpy/practic.py
+++ Machine-learning/numpy/practic.py
@@ -1,22 +1,2 @@
 import numpy as np
 
-# 1D Array Creation
-A = np.arange(3) # range for generate number in araray
-B = np.arange(0,20,2)
-C = np.linspace(1,4,1)
-
-print(C)
-print(B)
-print(A)
-
-# 2D Array Creation
-
-A2 = np.eye(3)
-B2 = np.eye(2,3)
-print(A2)
-print(B2)
-
-C2 = np.diag([1,2,3]) # for diagonal
-print(C2)
-D2 = np.array([[1,2,3],[4,5,6]])
-print(np.diag(D2)) # show D2's diagonal
