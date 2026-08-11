5c8df3091ad6474c050e18ae85a2bf1ceb456e9e
DashPgi <pajiparsa@gmail.com>
2026-08-12 00:19:49 +0330

    subset1

--- Machine-learning/Pandas/Doc.py
+++ Machine-learning/Pandas/Doc.py
@@ -50,3 +50,21 @@ data.to_csv("data.csv")
 data.to_json("data.json")
 
 # 3 - How do i select a subset of a DataFrame?
+
+missing_value = ["", " ", "0.0"]
+file = pd.read_csv("data.csv", na_values=missing_value) # -> Replacing mising file into NaN
+
+
+file.fillna(axis=0,inplace=True,value=np.nan)
+file.fillna(method = "ffill",axis=0,inplace=True) # -> Forward Fill out
+file.fillna(method = "bfill",axis=0,inplace=True) # -> backgward Fill out
+file.dropna(axis=0, how="any", inplace=True) # -> any => delete all row's have NaN | -> all =>Delete a row's have all Nan
+# linear interpolation => y = (y1+(x-x1))*(|y2-y1|/x2-x1)
+file.interpolate(method="linear", inplace=True)
+file.interpolate(method="polynomial", inplace=True)
+
+
+taintanic = pd.read_csv("taintanic.csv")
+children =taintanic.loc(taintanic["age"] < 15 , "name")
+adult = taintanic.loc(taintanic["age"] >20 , "name")
+old_citizen = taintanic.iloc[2:5,25:2]
