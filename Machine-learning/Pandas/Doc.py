dbb5b70806ca86c1eaf6b2f1a3f5fab73af74e4a
DashPgi <pajiparsa@gmail.com>
2026-08-13 16:51:27 +0330

    reshaping

--- Machine-learning/Pandas/Doc.py
+++ Machine-learning/Pandas/Doc.py
@@ -137,3 +137,26 @@ plt.ylabel("")
 plt.show()
 
 # ---- need more doc for that
+
+# How Creat new columns and calculate summary statistics
+
+data = pd.read_csv('data.csv').head()
+
+data["scale"] = data["Calories"] / 10
+data = data.rename(
+    columns={
+        "Calories": "Cal"
+    }
+)
+# Reshape the layout
+
+pivot = data.pivot(columns="Pulse", values="Cal")
+pivott = data.pivot_table(
+    values="Cal",
+    columns="Pulse",
+    index="scale",
+    aggfunc="mean"
+)
+malted = data.melt(id_vars="Pulse")
+print(pivott)
+
--- Machine-learning/Pandas/Pandas.py
+++ Machine-learning/Pandas/Pandas.py
@@ -1,24 +1,6 @@
 import pandas as pd
 import numpy as np
 
-# How Creat new columns and calculate summary statistics
+data = pd.read_csv('data.csv')
 
-data = pd.read_csv('data.csv').head()
-
-data["scale"] = data["Calories"] / 10
-data = data.rename(
-    columns={
-        "Calories": "Cal"
-    }
-)
-# Reshape the layout
-
-pivot = data.pivot(columns="Pulse", values="Cal")
-pivott = data.pivot_table(
-    values="Cal",
-    columns="Pulse",
-    index="scale",
-    aggfunc="mean"
-)
-malted = data.melt(id_vars="Pulse")
-print(pivott)
+# Mixing DataFrame 
