dc5c5de5bacb7ef8b7f77ccb3f9663f068401834
DashPgi <pajiparsa@gmail.com>
2026-08-17 22:34:08 +0330

    doc

---
+++ Machine-learning/Matplotlib-Seaborn/doc.py
@@ -0,0 +1,5 @@
+# Data Structure
+List = [1, 2, 3] # Changeable , Orderable , Repeatable
+Dict  = {"one": 1, "two": 2, "three": 3} # Keyable , Changeable , Orderable , Repeatable
+Tuple = (1, 2, 3)  # Orderable , Repeatable
+Set = {1, 2, 3} # Changeable
