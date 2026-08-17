809a6e17cc6c012122ece561680d70152a95c562
DashPgi <pajiparsa@gmail.com>
2026-08-17 23:01:14 +0330

    update2

--- Machine-learning/Matplotlib-Seaborn/mat.py
+++ Machine-learning/Matplotlib-Seaborn/mat.py
@@ -2,17 +2,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(-10, 10, 20)
-y = abs(x**2)
+y = abs(x)
 
 plt.plot(x, y,
          label="Graph",
-         color="blue",
+         color="black",
          marker="o",
+         linestyle="--",
          )
-plt.legend()
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+plt.grid(True,ls="--")
+plt.legend(loc="upper center")
+
+
+
 plt.title("This Is Title",
           fontsize=11,
           fontweight="bold",
-          color="blue",
+          color="black",
           )
 plt.show()
