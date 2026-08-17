e67975fa442d97f14a976ef2ce593ac759db024d
DashPgi <pajiparsa@gmail.com>
2026-08-18 00:11:11 +0330

    update6

--- Machine-learning/Matplotlib-Seaborn/doc.py
+++ Machine-learning/Matplotlib-Seaborn/doc.py
@@ -2,11 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Data Structure
-List = [1, 2, 3] # Changeable , Orderable , Repeatable
-Dict  = {"one": 1, "two": 2, "three": 3} # Keyable , Changeable , Orderable , Repeatable
+List = [1, 2, 3]  # Changeable , Orderable , Repeatable
+Dict = {"one": 1, "two": 2, "three": 3}  # Keyable , Changeable , Orderable , Repeatable
 Tuple = (1, 2, 3)  # Orderable , Repeatable
-Set = {1, 2, 3} # Changeable
+Set = {1, 2, 3}  # Changeable
+
 # Type of Matplotlib - > 1- HighLevel , 2- OOP
+
+# -- SimpleGrid
+
 # Highlevel :
 
 x = np.linspace(-10, 10, 20)
@@ -35,6 +39,29 @@ plt.title("This Is Title",
           color="black",
           )
 plt.show()
-plt.savefig("test.png",dpi=100)
+plt.savefig("test.png", dpi=100)
 
 # OOP :
+fig, ax = plt.subplots()
+
+ax.plot(x, y ** 3,
+        marker='o',
+        color='green',
+        linestyle='-.',
+        )
+
+plt.show()
+
+# -- Bar
+
+
+Country = ["Iran", "USA", "German", "Israel"]
+GDP = np.random.default_rng().integers(low=0, high=100, size=len(Country))
+
+bars = plt.bar(Country, GDP)  # bar -> vertical,barh -> horizontal
+bars[0].set_color('red')
+bars[1].set_hatch("/")
+bars[1].set_color('darkblue')
+bars[2].set_color('orange')
+bars[3].set_color('blue')
+plt.show()
--- Machine-learning/Matplotlib-Seaborn/mat.py
+++ Machine-learning/Matplotlib-Seaborn/mat.py
@@ -1,16 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(-10, 10, 20)
-y = abs(x)
-z = y + 3
+Country = ["Iran","USA","German","Israel"]
+GDP = np.random.default_rng().integers(low=0,high=100,size=len(Country))
 
-fig, ax = plt.subplots()
-
-ax.plot(x, y**3,
-        marker='o',
-        color='green',
-        linestyle='-.',
-        )
-
+bars = plt.bar(Country,GDP) # bar -> vertical,barh -> horizontal
+bars[0].set_color('red')
+bars[1].set_hatch("/")
+bars[1].set_color('darkblue')
+bars[2].set_color('orange')
+bars[3].set_color('blue')
 plt.show()
