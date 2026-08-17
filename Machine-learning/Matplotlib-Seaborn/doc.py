5e3bb399ee1516d0aa3627b907e8ea74c492f3ef
DashPgi <pajiparsa@gmail.com>
2026-08-18 00:23:16 +0330

    update7

--- Machine-learning/Matplotlib-Seaborn/doc.py
+++ Machine-learning/Matplotlib-Seaborn/doc.py
@@ -54,6 +54,7 @@ plt.show()
 
 # -- Bar
 
+# HL :
 
 Country = ["Iran", "USA", "German", "Israel"]
 GDP = np.random.default_rng().integers(low=0, high=100, size=len(Country))
@@ -64,4 +65,11 @@ bars[1].set_hatch("/")
 bars[1].set_color('darkblue')
 bars[2].set_color('orange')
 bars[3].set_color('blue')
+plt.show()
+
+# OOP :
+
+fig,ax = plt.subplots()
+
+ax.bar(Country,GDP)
 plt.show()
--- Machine-learning/Matplotlib-Seaborn/mat.py
+++ Machine-learning/Matplotlib-Seaborn/mat.py
@@ -4,10 +4,7 @@ import numpy as np
 Country = ["Iran","USA","German","Israel"]
 GDP = np.random.default_rng().integers(low=0,high=100,size=len(Country))
 
-bars = plt.bar(Country,GDP) # bar -> vertical,barh -> horizontal
-bars[0].set_color('red')
-bars[1].set_hatch("/")
-bars[1].set_color('darkblue')
-bars[2].set_color('orange')
-bars[3].set_color('blue')
+fig,ax = plt.subplots()
+
+ax.bar(Country,GDP)
 plt.show()
