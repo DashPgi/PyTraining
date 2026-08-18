552d9b5bfeea664d0893f44b8b02a28754e1f89f
DashPgi <pajiparsa@gmail.com>
2026-08-18 23:42:45 +0330

    update10

--- Machine-learning/Matplotlib-Seaborn/doc.py
+++ Machine-learning/Matplotlib-Seaborn/doc.py
@@ -123,8 +123,19 @@ fig, ax = plt.subplots()
 ax.fill_between(x=x, y1=y1, y2=y2,
                 color="skyblue",
                 alpha=0.5)
+
 ax.annotate("%x^2%",
-    xy=(3.5, 12),
-    xytext=(3, 1),
-    arrowprops=dict(arrowstyle="->"))
+            xy=(8, 100),
+            xytext=(3, 1),
+            arrowprops=dict(arrowstyle="->"))
+
+ax.annotate("%x^3%",
+            xy=(5, 125),
+            xytext=(7, 10),
+            arrowprops=dict(arrowstyle="->"))
+
 plt.show()
+
+# -- Histogram
+
+
--- Machine-learning/Matplotlib-Seaborn/mat.py
+++ Machine-learning/Matplotlib-Seaborn/mat.py
@@ -1,24 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(0, 10, 2)
-y1 = x ** 2
-y2 = x ** 3
+rand = np.random.randn(10000)
 
-fig, ax = plt.subplots()
+data2 = rand + 2
 
-ax.fill_between(x=x, y1=y1, y2=y2,
-                color="skyblue",
-                alpha=0.5)
-
-ax.annotate("%x^2%",
-            xy=(8, 100),
-            xytext=(3, 1),
-            arrowprops=dict(arrowstyle="->"))
-
-ax.annotate("%x^3%",
-            xy=(5, 125),
-            xytext=(7, 10),
-            arrowprops=dict(arrowstyle="->"))
+plt.hist(rand,
+         bins=50,  # -> Time to Repeat
+         edgecolor="black",
+         )
+plt.hist(data2,
+         edgecolor="white",
+         color="green",
+         bins=50,  # -> Time to Repeat
+         )
 
 plt.show()
