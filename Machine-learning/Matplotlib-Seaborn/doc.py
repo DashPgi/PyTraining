9bb3fab8b07411c9346799c7aa55ca8626b7846e
DashPgi <pajiparsa@gmail.com>
2026-08-20 15:11:38 +0330

    update15

--- Machine-learning/Matplotlib-Seaborn/doc.py
+++ Machine-learning/Matplotlib-Seaborn/doc.py
@@ -178,3 +178,11 @@ plt.show()
 
 # -- Heatmap
 
+
+matrix = np.random.default_rng().integers(low=0,high=10000,size=[25,25])
+
+plt.imshow(matrix)
+plt.xticks(range(5), list("ABCDE"))
+
+plt.colorbar()
+plt.show()
