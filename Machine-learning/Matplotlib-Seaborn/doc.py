0fdbadc8a4b8616781e84d05af6a0ebc43107354
DashPgi <pajiparsa@gmail.com>
2026-08-20 15:30:25 +0330

    update16

--- Machine-learning/Matplotlib-Seaborn/doc.py
+++ Machine-learning/Matplotlib-Seaborn/doc.py
@@ -188,3 +188,13 @@ plt.show()
 
 # -- Violin
 
+dt = np.random.normal(0, 1, 1000)
+dt2 = np.random.normal(1, 1.25, 1000)
+dt3 = np.random.normal(2, 1.75, 1000)
+plt.violinplot([dt, dt2, dt3],
+               showmeans=True,
+               showmedians=True)
+plt.show()
+
+# -- Counter
+
--- Machine-learning/Matplotlib-Seaborn/mat.py
+++ Machine-learning/Matplotlib-Seaborn/mat.py
@@ -1,10 +1,3 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-dt = np.random.normal(0, 1, 1000)
-dt2 = np.random.normal(1, 1.25, 1000)
-dt3 = np.random.normal(2, 1.75, 1000)
-plt.violinplot([dt, dt2, dt3],
-               showmeans=True,
-               showmedians=True)
-plt.show()
